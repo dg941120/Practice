20cac4e2491dc00a24526e027d2e564758f43350
Hong <vanguardv1023@gmail.com>
2020-06-17 14:38:08 +0900

    Rename, Move Classes

---
+++ Fibonacci_Numbers/Fibonacci_Numbers.java
@@ -0,0 +1,74 @@
+class Fib_30 {
+    public static void main(String[] args) {
+        int arr[] = new int[30]; // 30개의 배열을 만드는 것. 배열은 0 ~ 29가 생성.
+        int x = 1, y = 1, total = 0, loop = 1;
+        arr[0] = 1;
+        arr[1] = 1;
+
+        for(; loop < arr.length ; loop++) {
+            arr[loop] = total;
+            total = x + y;
+            x = y;
+            y = total;
+        }
+        System.out.println("30번째 항 : " + arr[29]);
+    }
+}
+
+
+import java.util.Scanner;
+
+class Fib_New {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("원하는 항을 입력하시오.");
+        int input = scan.nextInt();
+        int arr[] = new int[input + 1];             // ex) 인풋 = 2, 새로 만들 배열의 갯수 [3] (0, 1, 2)
+        int x = 1, y = 1, total = 1;
+
+        arr[0] = 1;
+        arr[1] = 1;
+
+        for(int loop = 1; loop < arr.length ; loop++) {     // loop == 1 < arr[2] = 0 이 될 때까지 loop
+            arr[loop] = total;                  // arr[2] = 1 == total = 1
+            total = x + y;                      // total = 1 == x = 1 + y = 1 --> total = 2
+            x = y;                              // x = 1 == y = 1
+            y = total;                          // y = 1 == total = 2 --> y == 2
+        }
+        System.out.println("원하는 항 : " + (arr[input - 1])); // 원하는 항 : arr[0]
+    }
+}
+
+
+import java.util.Scanner;
+
+class Fib_Num {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x = 1, y = 1, total = 0, loop = 2, want = 0;
+        System.out.println("몇번째 피보나치 항을 원하시나요?");
+        want = scan.nextInt();
+        if (want > 2) {
+            for (; ; loop++) {
+                if (want == loop) {
+                    break;
+                } else {
+                    total = x + y;
+                    x = y;
+                    y = total;
+                }
+            }
+            System.out.println("결과 값 : " + total);
+        } else if (want > 0) {
+            for (;want < 3;) {
+                total = x + y;
+                x = y;
+                y = total;
+                System.out.println("결과 값 : " + "1");
+                break;
+            }
+        } else {
+            System.out.println("잘못된 값을 입력했습니다.");
+        }
+    }
+}
--- TreeHit/TreeHit.java
+++ TreeHit/TreeHit.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TreeHit {
+class TreeHit {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int treeHit = 0;
@@ -22,3 +22,32 @@ public class TreeHit {
 }
 // https://wikidocs.net/212 해당 사이트의 예제를 내가 원하는 대로 바꾸어 보았다.
 // 원래는 treeHit++ 로 10까지 계속해서 수가 더해지는 것이었으나, if 로 사용자가 원할 때 오르게 바꾸었다.
+
+
+import java.util.Scanner;
+
+class TreeHit_Switch {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int treeHit = 0;
+        while (treeHit < 10) {
+            System.out.println("나무를 도끼로 찍으시겠습니까? Y/N");
+            String YorN = scan.next();
+            switch (YorN) {
+                case "Y":
+                    treeHit += 1;
+                    System.out.println("나무를 도끼로 찍었습니다. 지금까지 도끼로 나무를 " + treeHit + "번 찍었습니다.");
+                    continue;
+                case "N":
+                    System.out.println("나무를 도끼로 찍지 않았습니다. 지금까지 도끼로 나무를 " + treeHit + "번 찍었습니다.")
+                default:
+            }
+        }
+        if (treeHit == 10) {
+            System.out.println("나무가 넘어갑니다.");
+            // 구문 위치 실수가 있었다. 중괄호 위치를 잘 봐야함.
+        }
+    }
+}
+// https://wikidocs.net/212 해당 사이트의 예제를 내가 원하는 대로 바꾸어 보았다.
+// 원래는 treeHit++ 로 10까지 계속해서 수가 더해지는 것이었으나, while 과 switch 를 이용하여 사용자가 원할 때 오르게 바꾸었다.
