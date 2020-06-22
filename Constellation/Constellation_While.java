eda0f83f1941884bd499e3b9995defbd133b18c2
Hong <vanguardv1023@gmail.com>
2020-06-22 17:06:34 +0900

    Update Constellation_While.java

--- Constellation/Constellation_While.java
+++ Constellation/Constellation_While.java
@@ -82,97 +82,100 @@ class Constellation_While {
         }
 
         String Cons = "";
-        if (lastday > birth_day) {
-            if (birth_month == 1) {
-                if (birth_day >= 20) {
-                    Cons = "물병";
-                } else
-                    Cons = "염소";
+        while (true) {
+            if (lastday < birth_day) {
+                System.out.println("잘못 입력하셨습니다.");
+                System.out.println(birth_month + "월은 " + birth_day + "일이 존재하지 않습니다.");
+                System.out.println("태어난 일을 입력하세요.");
+                birth_day = scanner.nextInt();
+            } else {
+                if (birth_month == 1) {
+                    if (birth_day >= 20) {
+                        Cons = "물병";
+                    } else
+                        Cons = "염소";
+                }
+
+                if (birth_month == 2) {
+                    if (birth_day >= 19) {
+                        Cons = "물고기";
+                    } else
+                        Cons = "물병";
+                }
+
+                if (birth_month == 3) {
+                    if (birth_day >= 21) {
+                        Cons = "양";
+                    } else
+                        Cons = "물고기";
+                }
+
+                if (birth_month == 4) {
+                    if (birth_day >= 20) {
+                        Cons = "황소";
+                    } else
+                        Cons = "양";
+                }
+
+                if (birth_month == 5) {
+                    if (birth_day >= 21) {
+                        Cons = "쌍둥이";
+                    } else
+                        Cons = "황소";
+                }
+
+                if (birth_month == 6) {
+                    if (birth_day >= 22) {
+                        Cons = "게";
+                    } else
+                        Cons = "쌍둥이";
+                }
+
+                if (birth_month == 7) {
+                    if (birth_day >= 23) {
+                        Cons = "사자";
+                    } else
+                        Cons = "게";
+                }
+
+                if (birth_month == 8) {
+                    if (birth_day >= 23) {
+                        Cons = "처녀";
+                    } else
+                        Cons = "사자";
+                }
+
+                if (birth_month == 9) {
+                    if (birth_day >= 24) {
+                        Cons = "천칭";
+                    } else
+                        Cons = "처녀";
+                }
+
+                if (birth_month == 10) {
+                    if (birth_day >= 23) {
+                        Cons = "전갈";
+                    } else
+                        Cons = "천칭";
+                }
+
+                if (birth_month == 11) {
+                    if (birth_day >= 23) {
+                        Cons = "궁수";
+                    } else
+                        Cons = "전갈";
+                }
+
+                if (birth_month == 12) {
+                    if (birth_day >= 25) {
+                        Cons = "염소";
+                    } else
+                        Cons = "궁수";
+                }
+
+                System.out.println(name + "님의 별자리는 " + Cons + "자리입니다.");
+                break;
             }
-
-            if (birth_month == 2) {
-                if (birth_day >= 19) {
-                    Cons = "물고기";
-                } else
-                    Cons = "물병";
-            }
-
-            if (birth_month == 3) {
-                if (birth_day >= 21) {
-                    Cons = "양";
-                } else
-                    Cons = "물고기";
-            }
-
-            if (birth_month == 4) {
-                if (birth_day >= 20) {
-                    Cons = "황소";
-                } else
-                    Cons = "양";
-            }
-
-            if (birth_month == 5) {
-                if (birth_day >= 21) {
-                    Cons = "쌍둥이";
-                } else
-                    Cons = "황소";
-            }
-
-            if (birth_month == 6) {
-                if (birth_day >= 22) {
-                    Cons = "게";
-                } else
-                    Cons = "쌍둥이";
-            }
-
-            if (birth_month == 7) {
-                if (birth_day >= 23) {
-                    Cons = "사자";
-                } else
-                    Cons = "게";
-            }
-
-            if (birth_month == 8) {
-                if (birth_day >= 23) {
-                    Cons = "처녀";
-                } else
-                    Cons = "사자";
-            }
-
-            if (birth_month == 9) {
-                if (birth_day >= 24) {
-                    Cons = "천칭";
-                } else
-                    Cons = "처녀";
-            }
-
-            if (birth_month == 10) {
-                if (birth_day >= 23) {
-                    Cons = "전갈";
-                } else
-                    Cons = "천칭";
-            }
-
-
-            if (birth_month == 11) {
-                if (birth_day >= 23) {
-                    Cons = "궁수";
-                } else
-                    Cons = "전갈";
-            }
-
-            if (birth_month == 12) {
-                if (birth_day >= 25) {
-                    Cons = "염소";
-                } else
-                    Cons = "궁수";
-            }
-
-            System.out.println(name + "님의 별자리는 " + Cons + "자리입니다.");
-        } else {
-            System.out.println("잘못 입력하셨습니다.");
-            System.out.println(birth_month + "월은 " + birth_day + "일이 존재하지 않습니다.");
-            return;
         }
     }
 }
